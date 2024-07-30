66dbe3e06bfb02b4642fb03ad99513acf115670c
Tanishq84 <tanishqsom_19@outlook.com>
2024-07-30 15:52:00 +0530

    New

--- main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Text
-
-# Create your views here.
-def maintainence(request):
-    text = Text.objects.all()
-    return render(request, 'index.html', {'text': text})
--- maintain/settings.py
+++ maintain/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_uc=$y&s&%w_u=d-46g*9h++p7-hvxf81omumku7l5ipd9!i#="
+SECRET_KEY = "django-insecure-h%(s!ykkv-k*mca66yzl4#^vy@ml5taq^$+a))77^e#e6a*obu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "main",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
